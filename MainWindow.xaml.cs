0e342fc961af587cbcb0d1d8bed9bf86bd159196
ikriv <ivan@ikriv.com>
2017-12-21 16:49:57 -0500

    Initial commit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfStdOut
 {
@@ -23,6 +11,13 @@ namespace WpfStdOut
         public MainWindow()
         {
             InitializeComponent();
+            OutputTextBox.Focus();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var line = OutputTextBox.Text;
+            Console.Out.WriteLine(line);
         }
     }
 }
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WpfStdOut
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
